e2ed47d7125092fba33fec23b7144f8e7fe4dd47
securemist <gongjiatian00@qq.com>
2023-08-25 18:48:39 +0800

    push

---
+++ src/main/java/com/mist/cloud/domain/file/model/vo/FileVo.java
@@ -0,0 +1,9 @@
+package com.mist.cloud.domain.file.model.vo;/**
+*@Author: securemist
+*@Datetime: 2023/8/24 14:13
+*@Description:
+
+
+*/
+public class FileVo {
+}
---
+++ src/main/java/com/mist/cloud/domain/file/model/vo/FolderVo.java
@@ -0,0 +1,9 @@
+package com.mist.cloud.domain.file.model.vo;/**
+*@Author: securemist
+*@Datetime: 2023/8/24 14:12
+*@Description:
+
+
+*/
+public class FolderVo {
+}
