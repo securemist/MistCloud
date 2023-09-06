c78a9b45365c843502a5c06ec58d24cbc1953b2a
securemist <gongjiatian00@qq.com>
2023-09-06 20:24:44 +0800

    remove web project

--- web-react/src/api/type.ts
+++
@@ -1,4 +0,0 @@
-export default interface CommonResponse {
-    code: number,
-    msg: string
-}
--- web-react/src/layout/footer/index.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./footer.module.scss";
-export function Footer() {
-    return (
-        <div className={styles.footer}>
-                <span>powered by <a href="https://github.com/securemist">securemist</a></span>
-        </div>
-    )
-}
